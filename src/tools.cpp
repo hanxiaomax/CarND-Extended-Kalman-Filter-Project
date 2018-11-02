5a6e3b3b30066b8ffb741ae9ac4d89b191cbbf87
hanxiaomax@qq.com <hanxiaomax@qq.com>
2018-11-02 14:05:04 +0000

    finish ekf

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,10 +11,26 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+  //check the input
+  if (((estimations.size() != ground_truth.size())|| estimations.size() == 0))
+  {
+      std::cout << "estimation or ground_truth invalid" << std::endl;
+      return rmse;
+  }
+  for (unsigned int i = 0; i < estimations.size(); ++i){
+
+      VectorXd delta = estimations[i] - ground_truth[i];
+
+      delta = delta.array()*delta.array();
+      rmse += delta;
+  }
+
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +38,24 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3, 4);
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+
+  if (fabs(c1) < 0.0001){
+      std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+      return Hj;
+  }
+
+  Hj << (px / c2), (py / c2), 0, 0,
+      -(py / c1), (px / c1), 0, 0,
+      py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
+
+  return Hj;
 }
